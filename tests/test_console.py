c099b8f1270424eaad6758d12d0631e27be068c1
jovynina1 <jovynina@gmail.com>
2023-03-11 11:45:03 +0000

    AinBnB CLONE

---
+++ tests/test_console.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+"""Defines unittests for console.py.
+"""
+from io import StringIO
+import os
+import unittest
+from unittest.mock import patch
+from console import HBNBCommand
+from models import storage
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class TestConsole(unittest.TestCase):
+        """Base class for testing Console.
+            """
+
+        def setUp(self):
+            pass
+
+        def tearDown(self) -> None:
+            """Resets FileStorage data."""
+            storage._FileStorage__objects = {}
+            if os.path.exists(storage._FileStorage__file_path):
+                os.remove(storage._FileStorage__file_path)
+
+        def test_simple(self):
+            """Tests basic commands.
+            """
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("quit")
+            self.assertEqual(f.getvalue(), "")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("EOF")
+            self.assertEqual(f.getvalue(), "\n")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("\n")
+            self.assertEqual(f.getvalue(), "")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("?")
+            self.assertIsInstance(f.getvalue(), str)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help")
+            self.assertIsInstance(f.getvalue(), str)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("? create")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(), "Creates a new instance.")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help create")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(), "Creates a new instance.")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("? all")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(),
+                            "Prints string representation of all instances.")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help all")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(),
+                            "Prints string representation of all instances.")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            msg = "Prints the string representation of an instance."
+            HBNBCommand().onecmd("? show")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(),
+                            msg)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            msg = "Prints the string representation of an instance."
+            HBNBCommand().onecmd("help show")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(),
+                            msg)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            msg = "Updates an instance based on the class name and id."
+            HBNBCommand().onecmd("? update")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(),
+                            msg)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            msg = "Updates an instance based on the class name and id."
+            HBNBCommand().onecmd("help update")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(),
+                            msg)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            msg = "Deletes an instance based on the class name and id."
+            HBNBCommand().onecmd("? destroy")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(),
+                            msg)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            msg = "Deletes an instance based on the class name and id."
+            HBNBCommand().onecmd("help destroy")
+            self.assertIsInstance(f.getvalue(), str)
+            self.assertEqual(f.getvalue().strip(), msg)
+
+
+
+
